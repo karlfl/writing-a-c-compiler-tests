cecad4e36b4e0de4c0f06ee11b369c4c72cecd82
Karl Fleischmann <karlf@live.com>
2024-11-11 19:29:00 +0000

    Initial setup

---
+++ myc/Program.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+// program file path required as argument
+if(args.Length == 0) {
+    Console.WriteLine("\nProgram File path missing from arguments\n");
+    return -1;
+}
+
+string programFile = args[0];
+Console.WriteLine("\nProgram File: {0}\n", programFile);
+
+return 0;
