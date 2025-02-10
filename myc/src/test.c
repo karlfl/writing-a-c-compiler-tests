e720d6307b68e26a21a4f59854da2da7baa455f2
Karl Fleischmann <karlf@live.com>
2025-02-10 02:24:54 +0000

    Chapter 5 Compound Lex

--- myc/src/test.c
+++ myc/src/test.c
@@ -1,11 +1,7 @@
 int main(void) {
-    int a = 10;
-    int b = 20;
-    for (b = -20; b < 0; b = b + 1) {
-        a = a - 1;
-        if (a <= 0)
-            break;
-    }
-
-    return a == 0 && b == -11;
+    int a = 1;
+    int b = 2;
+    int c = ++a;
+    int d = --b;
+    return (a == 2 && b == 1 && c == 2 && d == 1);
 }
